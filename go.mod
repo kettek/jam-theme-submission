614f3b7ba78773d2c8257c001e04a9dd6b42d142
kts of kettek <kts@kettek.net>
2023-12-14 21:14:08 -0800

    Add working code for voting

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/kettek/jam-theme-submission
+
+go 1.20
+
+require github.com/steambap/captcha v1.4.1
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
+)
---
+++ main.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"text/template"
+	"time"
+
+	"github.com/steambap/captcha"
+)
+
+// Settings contains the theme submission settings.
+type Settings struct {
+	Title      string
+	Text       string
+	Entries    int
+	UseCaptcha bool
+	UseHeader  bool
+}
+
+type Entries map[string]int
+
+var entries Entries
+var saveLock sync.Mutex
+var settings Settings
+var captchaLock sync.Mutex
+var lastCaptchaTime time.Time
+var lastCaptchaData *captcha.Data
+var funcMap template.FuncMap
+
+type templateSettings struct {
+	Title           string
+	Text            string
+	Submissions     []string
+	SubmissionCount int
+	UseHeader       bool
+	UseCaptcha      bool
+	CaptchaFailed   bool
+	IsSubmission    bool
+	IsResults       bool
+	Results         Entries
+}
+
+func init() {
+	entries = make(Entries)
+	funcMap = template.FuncMap{
+		"inc": func(i int) int {
+			return i + 1
+		},
+	}
+	settings.Entries = 4
+}
+
+func main() {
+	if err := loadSettings(); err != nil {
+		fmt.Println(err)
+		settings.Entries = 4
+		settings.Title = "Game Jam"
+		settings.UseCaptcha = true
+		if err := saveSettings(); err != nil {
+			fmt.Println(err)
+		}
+	}
+	if err := loadEntries(); err != nil {
+		fmt.Println(err)
+	}
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/captcha", handleCaptcha)
+	http.HandleFunc("/results", handleResults)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
+
+func handleResults(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.New("index.html").Funcs(funcMap).ParseFiles("index.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	if err := tmpl.Execute(w, templateSettings{
+		IsResults: true,
+		UseHeader: settings.UseHeader,
+		Text:      settings.Text,
+		Title:     settings.Title,
+		Results:   entries,
+	}); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	var err error
+	if settings.UseCaptcha {
+		captchaLock.Lock()
+		defer captchaLock.Unlock()
+		if time.Since(lastCaptchaTime) >= 5*time.Minute {
+			lastCaptchaData, err = captcha.New(150, 50)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			lastCaptchaTime = time.Now()
+		}
+	}
+
+	var s templateSettings
+	s.UseHeader = settings.UseHeader
+	s.UseCaptcha = settings.UseCaptcha
+	s.Text = settings.Text
+	s.Title = settings.Title
+	if r.Method == "GET" {
+		tmpl, err := template.New("index.html").Funcs(funcMap).ParseFiles("index.html")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		s.Submissions = make([]string, settings.Entries) // 4 for now...
+		if err := tmpl.Execute(w, s); err != nil {
+			fmt.Println(err)
+		}
+		return
+	} else if r.Method == "POST" {
+		if err := r.ParseForm(); err != nil {
+			fmt.Println(err)
+			return
+		}
+		for k, v := range r.PostForm {
+			if k == "submission[]" {
+				s.Submissions = append(s.Submissions, v...)
+			} else if k == "captcha" {
+				if v[0] != lastCaptchaData.Text {
+					s.CaptchaFailed = true
+				}
+			}
+		}
+		if !settings.UseCaptcha || !s.CaptchaFailed {
+			for _, v := range s.Submissions {
+				v = strings.TrimSpace(strings.ToLower(v))
+				if v == "" {
+					continue
+				}
+				entries[v]++
+				s.SubmissionCount++
+			}
+			saveEntries()
+		}
+		s.IsSubmission = true
+		tmpl, err := template.New("index.html").Funcs(funcMap).ParseFiles("index.html")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		tmpl.Execute(w, s)
+		return
+	}
+	// 500 or w/e
+}
+
+func handleCaptcha(w http.ResponseWriter, r *http.Request) {
+	if !settings.UseCaptcha {
+		return
+	}
+	lastCaptchaData.WriteImage(w)
+}
+
+func loadSettings() error {
+	bytes, err := os.ReadFile("settings.json")
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bytes, &settings)
+}
+
+func saveSettings() error {
+	bytes, err := json.Marshal(settings)
+	if err != nil {
+		return err
+	}
+	if err := os.WriteFile("settings.json", bytes, 0644); err != nil {
+		return err
+	}
+	return nil
+}
+
+func loadEntries() error {
+	bytes, err := os.ReadFile("entries.json")
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bytes, &entries)
+}
+
+func saveEntries() error {
+	saveLock.Lock()
+	defer saveLock.Unlock()
+	bytes, err := json.Marshal(entries)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile("entries.json", bytes, 0644)
+}
